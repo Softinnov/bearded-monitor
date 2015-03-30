cc990916c9325e9593bd220659b9313863017b11
Ghislain Guiot <gt.ghislain@gmail.com>
2015-03-30 12:03:54 +0200

    change duration to interval

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ Lean, fast, flexible monitoring
 Usage
 =====
 ```bash
-$ ./bearded-monitor -p 50 -d 5s -s usr1 to_watch to_see
+$ ./bearded-monitor -p 50 -i 5s -s usr1 to_watch to_see
 2015/03/27 16:08:48 Looking for commands containing: ["to_watch" "to_see"]
-2015/03/27 16:08:48 Duration fixed to: 5s
+2015/03/27 16:08:48 Interval fixed to: 5s
 2015/03/27 16:08:48 Looking for %CPU usage higher than 50%
 2015/03/27 16:08:48 Kill signal to send: "user defined signal 1"
 
--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 var (
 	fsys = flag.String("s", "usr1", "kill syscall sent")
 	fper = flag.Float64("p", 90, "percentage threshold before sending a kill signal")
-	fdur = flag.Duration("d", time.Duration(time.Minute), "duration")
+	fdur = flag.Duration("i", time.Duration(time.Minute), "interval of each check")
 
 	selfPID int = os.Getpid()
 )
@@ -66,7 +66,7 @@ func main() {
 		log.Fatal("Signal not recognised")
 	}
 	log.Printf("Looking for commands containing: %q\n", flag.Args())
-	log.Printf("Duration fixed to: %v\n", *fdur)
+	log.Printf("Interval fixed to: %v\n", *fdur)
 	log.Printf("Looking for %%CPU usage higher than %v%%\n", *fper)
 	log.Printf("Kill signal to send: %q\n\n", sysc[*fsys])
 
